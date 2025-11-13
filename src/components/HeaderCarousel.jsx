88cf14fa347c439f7d7c6b7cc796a6c33a6d58cb
olim2 <olim@bnav.uz>
2025-11-14 00:22:10 +0500

    home

--- src/components/HeaderCarousel.jsx
+++ src/components/HeaderCarousel.jsx
@@ -4,7 +4,7 @@ import React from "react";
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/autoplay';
-import carousel1 from "../assets/img/carousel-1.jpg"
+import carousel1 from "../assets/logo.jpg"
 import {useTranslation} from "react-i18next";
 import {Link} from "react-router-dom";
 
@@ -13,30 +13,27 @@ const HeaderCarousel = () => {
 
 
     return (
-        <div id='header-carousel' className="container-fluid p-0 wow fadeIn" data-wow-delay="0.1s"
-             style={{position: 'relative'}}>
-
-            <div className="position-relative">
-                <img className="w-100 carousel-image" src={carousel1} alt={`slide`}/>
-                <div className="carousel-caption">
-                    <div className="container">
-                        <div className="row justify-content-center">
-                            <div className="col-lg-7 text-center">
-                                <h1 className="display-5 text-light mb-2 animate__animated animate__slideInDown">
-                                    {t("slideText")}
-                                </h1>
-                                <h1 className="text-light mb-3 mb-md-5 animate__animated animate__slideInDown">{t("slideText1")}
-                                </h1>
-                                {/*<Link to="/about"*/}
-                                {/*      className="btn btn-primary py-sm-2 py-md-3 px-sm-5">{t("learnMore")}</Link>*/}
-                                {/*<Link to="/training"*/}
-                                {/*      className="btn btn-light py-sm-2 py-md-3 px-sm-5 ms-3">{t("ourCourses")}</Link>*/}
-                            </div>
+        <div id='header-carousel' className="container-fluid p-0 wow fadeIn" data-wow-delay="0.1s">
+            <div className="carousel-caption">
+                <div className="container">
+                    <div className="row justify-content-center">
+                        <div className='col-8 col-lg-6 mb-5'>
+                            <img className="w-100" src={carousel1} alt={`slide`}/>
+                        </div>
+                        <div className="col-12 text-center mt-5">
+                            <h1 className="display-md-6 display-5 fst-italic mb-2 animate__animated animate__slideInDown">
+                                {t("title")}
+                            </h1>
+                            <p className="mb-3 text-dark display-6 fw-light mb-md-5 animate__animated animate__slideInDown">{t("subtitle")}
+                            </p>
+                            {/*<Link to="/about"*/}
+                            {/*      className="btn btn-primary py-sm-2 py-md-3 px-sm-5">{t("learnMore")}</Link>*/}
+                            {/*<Link to="/training"*/}
+                            {/*      className="btn btn-light py-sm-2 py-md-3 px-sm-5 ms-3">{t("ourCourses")}</Link>*/}
                         </div>
                     </div>
                 </div>
             </div>
-
         </div>
     );
 };
